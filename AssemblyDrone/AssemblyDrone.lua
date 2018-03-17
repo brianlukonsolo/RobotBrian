ae70b0a8b4491de83c38b6c6b1daf6193836eea8
brianlukonsolo139 <brian.lukonsolo@gmail.com>
2018-03-17 23:09:18 +0000

    Refactored AssemblerDrone.lua to separate out some logic.
    - Added new function called checkFailures

--- AssemblyDrone/AssemblyDrone.lua
+++ AssemblyDrone/AssemblyDrone.lua
@@ -24,21 +24,26 @@ function AssemblyDrone.buildRobot(brainModule, leftArmModule, rightArmModule, bo
       armsResult = pcall( function() self.leftArm.move(0,0,0); self.rightArm.move(0,0,0) end)
       bodyResult = pcall( function() self.body.turn(45); self.body.turn(135) end)
       legsResult = pcall( function() self.leftLeg.moveFoward(0, 0); self.rightLeg.moveFoward(0, 0) end)
-  
-        if brainResult and armsResult and bodyResult and legsResult then
-          print(constants.robotName .. 'Self check successful. All modules online.' .. constants.newLine)
-        else
-          print(constants.robotName .. 'Warning: Self check failures!')
-          if not brainResult then print(constants.robotName .. constants.BRAIN_FAILURE) end
-          if not armsResult then print(constants.robotName .. constants.ARMS_FAILURE) end
-          if not bodyResult then print(constants.robotName .. constants.BODY_FAILURE) end
-          if not legsResult then print(constants.robotName .. constants.LEGS_FAILURE) end
-        end
+      --Check that all robot parts are online and ready
+      checkFailures(brainResult, armsResult, bodyResult, legsResult)
+      
     end
   
   return robot
   
 end
 
+function checkFailures(brainResult, armsResult, bodyResult, legsResult)
+  if brainResult and armsResult and bodyResult and legsResult then
+    print(constants.robotName .. 'Self check successful. All modules online.' .. constants.newLine)
+  else
+    print(constants.robotName .. 'Warning: Self check failures!')
+    if not brainResult then print(constants.robotName .. constants.BRAIN_FAILURE) end
+    if not armsResult then print(constants.robotName .. constants.ARMS_FAILURE) end
+    if not bodyResult then print(constants.robotName .. constants.BODY_FAILURE) end
+    if not legsResult then print(constants.robotName .. constants.LEGS_FAILURE) end
+  end
+  
+end
 
 return AssemblyDrone
